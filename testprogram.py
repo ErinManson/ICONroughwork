34c4ff2193f37d78a68fffedce5e93d07f6df949
ErinManson <0404em@gmail.com>
2024-06-10 13:37:34 -0400

    added testprogram.py

---
+++ testprogram.py
@@ -0,0 +1,18 @@
+import cohere # type: ignore
+co = cohere.Client('8xAa7F7OMYGK3sZPj3q0dzh8AQd41PjiDwlzQ5Wh')
+
+def generate(prompt, temp=0):
+    response = co.chat_stream(
+        message=prompt,
+        model="command-r",
+        temperature=temp,
+        preamble='')
+    
+    for event in response:
+        if event.event_type == "text-generation":
+            print(event.text, end='')
+            
+user_input = "a pizza named the EEM"
+prompt = f"""Write a one sentence product description for {user_input}"""
+
+generate(prompt, temp=0.5)
