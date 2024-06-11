848060823aa953aaa9ea1663e9b5a74ff97a0798
ErinManson <0404em@gmail.com>
2024-06-11 10:04:51 -0400

    now prints titles of postings

--- RSSFeed.py
+++ RSSFeed.py
@@ -2,4 +2,8 @@ import feedparser
 NewsFeed = feedparser.parse("file:///home/coop/search-feed%20.xml")
 entry = NewsFeed.entries[1]
 
+print('Number of RSS posts :', len(NewsFeed.entries))
 print(entry.keys())
+print("Title:")
+for i in NewsFeed.entries:
+    print(entry.title)
