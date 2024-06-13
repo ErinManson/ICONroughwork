a5253e9cf74c0ea0c423fbd9d5dd5e12acb1a96d
ErinManson <0404em@gmail.com>
2024-06-13 15:26:14 -0400

    finished chapter 2

--- huggingFaceCh2.py
+++ huggingFaceCh2.py
@@ -1,9 +1,11 @@
-from transformers import BertConfig, BertModel
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-# Building the config
-config = BertConfig()
+checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint)
+sequences = ["I've been waiting for a HuggingFace course my whole life.", "So have I!"]
 
-# Building the model from the config
-model = BertModel(config)
-
-print(config)
+tokens = tokenizer(sequences, padding=True, truncation=True, return_tensors="pt")
+output = model(**tokens)
+print(output)
