72543f3616c7b1eb4a01e755c2a607a85bac3775
ErinManson <0404em@gmail.com>
2024-06-13 16:58:54 -0400

    updates

--- huggingFaceCh3.py
+++ huggingFaceCh3.py
@@ -2,3 +2,7 @@ from datasets import load_dataset
 
 raw_datasets = load_dataset("glue", "mrpc")
 print(raw_datasets)
+
+raw_train_dataset = raw_datasets["train"]
+print(raw_train_dataset[15])
+print(raw_train_dataset.features)
