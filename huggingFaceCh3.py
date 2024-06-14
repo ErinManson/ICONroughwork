42f862d99dbe9e7e68141eb2f802c92e21d4eadc
ErinManson <0404em@gmail.com>
2024-06-14 10:27:40 -0400

    pausing for the day

--- huggingFaceCh3.py
+++ huggingFaceCh3.py
@@ -1,8 +1,22 @@
+from transformers import AutoTokenizer
 from datasets import load_dataset
+from transformers import DataCollatorWithPadding
+
 
 raw_datasets = load_dataset("glue", "mrpc")
-print(raw_datasets)
+raw_datasets
 
-raw_train_dataset = raw_datasets["train"]
-print(raw_train_dataset[15])
-print(raw_train_dataset.features)
+checkpoint = "bert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+def tokenize_function(example):
+    return tokenizer(example["sentence1"], example["sentence2"], truncation=True)
+
+tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+tokenized_datasets
+samples = tokenized_datasets["train"][:8]
+samples = {k: v for k, v in samples.items() if k not in ["idx", "sentence1", "sentence2"]}
+print([len(x) for x in samples["input_ids"]])
+batch = data_collator(samples)
+print({k: v.shape for k, v in batch.items()})
