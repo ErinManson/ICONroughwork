270b57aff782a77903fd9ad2312e88d880e2878d
ErinManson <0404em@gmail.com>
2024-06-17 16:57:33 -0400

    Edited

--- huggingFaceCh3.py
+++ huggingFaceCh3.py
@@ -2,6 +2,10 @@ from datasets import load_dataset
 from transformers import AutoTokenizer, DataCollatorWithPadding
 from transformers import TrainingArguments
 from transformers import AutoModelForSequenceClassification
+from transformers import Trainer
+import numpy as np
+import evaluate
+
 
 
 raw_datasets = load_dataset("glue", "mrpc")
@@ -18,4 +22,23 @@ data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 training_args = TrainingArguments("test-trainer")
 model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=2)
 
+def compute_metrics(eval_preds):
+    metric = evaluate.load("glue", "mrpc")
+    logits, labels = eval_preds
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
 
+training_args = TrainingArguments("test-trainer", evaluation_strategy="epoch")
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=2)
+
+trainer = Trainer(
+    model,
+    training_args,
+    train_dataset=tokenized_datasets["train"],
+    eval_dataset=tokenized_datasets["validation"],
+    data_collator=data_collator,
+    tokenizer=tokenizer,
+    compute_metrics=compute_metrics,
+)
+
+trainer.train()
